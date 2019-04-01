1c302c856aa57d8cab8154296ffebc6dcb017623
Alex Kagarov <alexkagarov@mbp-dima.rusanovka-net.kiev.ua>
2019-04-01 20:43:13 +0300

    Initial Commit

---
+++ Homework-Alerts-Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework-Alerts-Git
+//
+//  Created by Alex Kagarov on 4/1/19.
+//  Copyright © 2019 Alex Kagarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
